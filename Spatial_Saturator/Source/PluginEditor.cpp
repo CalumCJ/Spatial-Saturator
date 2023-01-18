a69a1714d11da4511fa0e2c79198501a0b097efb
COMPUTERNAME\CJ <calumljamieson@gmail.com>
2023-01-18 21:50:52 +0000

    feature/inital_commit: First commit of prototype code for the Spatial_Saturator - Includes processing of mixing left/right into mids/sides - Applies mid and sides filters - Code compiles though VST still to be tested. - Saturator and Limiter still to be added

---
+++ Spatial_Saturator/Source/PluginEditor.cpp
@@ -0,0 +1,101 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SpatialSaturatorAudioProcessorEditor::SpatialSaturatorAudioProcessorEditor(SpatialSaturatorAudioProcessor& p, juce::AudioProcessorValueTreeState& vts)
+    : AudioProcessorEditor(&p), audioProcessor(p), treeState(vts)
+{
+    midGainSlider.setTextValueSuffix(" dB ");
+    addAndMakeVisible(midGainSlider);
+    midGainSliderAttachment.reset(new SliderAttachment(treeState, "midGainID", midGainSlider));
+    addAndMakeVisible(midGainSliderLabel);
+    midGainSliderLabel.setText("mid Gain", juce::dontSendNotification);
+    midGainSliderLabel.attachToComponent(&midGainSlider, true);
+
+    midFreqSlider.setTextValueSuffix(" Hz ");
+    addAndMakeVisible(midFreqSlider);
+    midFreqSliderAttachment.reset(new SliderAttachment(treeState, "midFreqID", midFreqSlider));
+    addAndMakeVisible(midFreqSliderLabel);
+    midFreqSliderLabel.setText("mid Frequency", juce::dontSendNotification);
+    midFreqSliderLabel.attachToComponent(&midFreqSlider, true);
+
+    sideGainSlider.setTextValueSuffix(" dB ");
+    addAndMakeVisible(sideGainSlider);
+    sideGainSliderAttachment.reset(new SliderAttachment(treeState, "sideGainID", sideGainSlider));
+    addAndMakeVisible(sideGainSliderLabel);
+    sideGainSliderLabel.setText("side Gain", juce::dontSendNotification);
+    sideGainSliderLabel.attachToComponent(&sideGainSlider, true);
+
+    sideFreqLowerSlider.setTextValueSuffix(" Hz ");
+    addAndMakeVisible(sideFreqLowerSlider);
+    sideFreqLowerSliderAttachment.reset(new SliderAttachment(treeState, "sideFreqLowerID", sideFreqLowerSlider));
+    addAndMakeVisible(sideFreqLowerSliderLabel);
+    sideFreqLowerSliderLabel.setText("Lower side Frequency", juce::dontSendNotification);
+    sideFreqLowerSliderLabel.attachToComponent(&sideFreqLowerSlider, true);
+
+    sideFreqUpperSlider.setTextValueSuffix(" Hz ");
+    addAndMakeVisible(sideFreqUpperSlider);
+    sideFreqUpperSliderAttachment.reset(new SliderAttachment(treeState, "sideFreqUpperID", sideFreqUpperSlider));
+    addAndMakeVisible(sideFreqUpperSliderLabel);
+    sideFreqUpperSliderLabel.setText("Higher side Frequency", juce::dontSendNotification);
+    sideFreqUpperSliderLabel.attachToComponent(&sideFreqUpperSlider, true);
+
+    makeUpGainSlider.setTextValueSuffix(" dB ");
+    addAndMakeVisible(makeUpGainSlider);
+    makeUpGainSliderAttachment.reset(new SliderAttachment(treeState, "makeUpGainID", makeUpGainSlider));
+    addAndMakeVisible(makeUpGainSliderLabel);
+    makeUpGainSliderLabel.setText("Make Up Gain", juce::dontSendNotification);
+    makeUpGainSliderLabel.attachToComponent(&makeUpGainSlider, true);
+
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize(800, 600);
+}
+
+SpatialSaturatorAudioProcessorEditor::~SpatialSaturatorAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void SpatialSaturatorAudioProcessorEditor::paint(juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+}
+
+void SpatialSaturatorAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+
+    auto sliderLeft = 250;
+    auto sliderHeight = 20;
+
+    int numSliders = 6;
+    int N = 1;
+    int step = (getHeight() / (numSliders + 1));
+
+    midGainSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+    midFreqSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+    sideGainSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+    sideFreqLowerSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+    sideFreqUpperSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+    makeUpGainSlider.setBounds(sliderLeft, (N++ * step) - (sliderHeight / 2), getWidth() - sliderLeft * 1.5, sliderHeight);
+}
